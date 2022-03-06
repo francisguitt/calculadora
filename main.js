17bd46ea670bfe890e82c7b2184acbebfdceb3b5
francisguitt <77627405+francisguitt@users.noreply.github.com>
2022-03-06 18:05:45 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,10 +5,20 @@ function sol(){
   let x =
   document.getElementById('result').value;
   let y = eval(x) 
+  if(isFinite(y)){
   document.getElementById('result').value=y;
+  }
+  else{
+     erro('Não e possivel ÷ por 0');
+  }
+ 
 }
 function clr(){
   document.getElementById('result').value='';
 }
 
- 
+ function error(msg){
+ const Error = msg;
+   document.getElementById('result').value=Error;
+   
+ }
